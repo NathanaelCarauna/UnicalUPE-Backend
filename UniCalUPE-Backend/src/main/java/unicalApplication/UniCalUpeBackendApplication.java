cacbfe9b71c896589ab3adf2e9601f51bf5138f7
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-11-18 17:38:48 -0300

    Swagger added to project

--- UniCalUPE-Backend/src/main/java/unicalApplication/UniCalUpeBackendApplication.java
+++ UniCalUPE-Backend/src/main/java/unicalApplication/UniCalUpeBackendApplication.java
@@ -9,5 +9,5 @@ public class UniCalUpeBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UniCalUpeBackendApplication.class, args);
 	}
-
+	
 }
--- UniCalUPE-Backend/src/main/java/unicalApplication/config/SwaggerConfig.java
+++ UniCalUPE-Backend/src/main/java/unicalApplication/config/SwaggerConfig.java
@@ -7,9 +7,11 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class SpringFoxConfig {
+@EnableSwagger2
+public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
@@ -18,4 +20,5 @@ public class SpringFoxConfig {
 				.paths(PathSelectors.any())
 				.build();
 	}
+	
 }

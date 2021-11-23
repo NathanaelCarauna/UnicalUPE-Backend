679ccef3a5fc60e2b49a4ea3e3ae76e791e344dd
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-11-22 22:16:09 -0300

    Event entity implemented

---
+++ UniCalUPE-Backend/src/main/java/unicalApplication/enums/Category.java
@@ -0,0 +1,5 @@
+package unicalApplication.enums;
+
+public enum Category {
+	EVENT, SURVEY
+}
--- UniCalUPE-Backend/src/main/java/unicalApplication/models/Event.java
+++ UniCalUPE-Backend/src/main/java/unicalApplication/models/Event.java
@@ -1,11 +1,14 @@
 package unicalApplication.models;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
+import unicalApplication.enums.Category;
 
 @Entity
 @Data
@@ -13,4 +16,12 @@ public class Event {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
+	private String title;
+	
+	private Category category;
+	private String palestrante;
+	
+	private Date startDateTime;
+	private Date endDateTime;
 }

36af7b16588eee266ac5b548e4668f82a87601ce
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-11-22 21:51:19 -0300

    Relationship user-notifications implemented

--- UniCalUPE-Backend/src/main/java/unicalApplication/models/Notification.java
+++ UniCalUPE-Backend/src/main/java/unicalApplication/models/Notification.java
@@ -7,7 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -23,6 +26,10 @@ public class Notification {
 
 	@OneToOne
 	private Event event;
+	
+	@ManyToOne
+	@JsonIgnore
+	private UserEntity user;
 
 	private Date creationTime;
 
--- UniCalUPE-Backend/src/main/java/unicalApplication/models/UserEntity.java
+++ UniCalUPE-Backend/src/main/java/unicalApplication/models/UserEntity.java
@@ -6,25 +6,26 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import lombok.Data;
 import unicalApplication.enums.AccountType;
 
 @Entity
 @Data
-public class User {
+public class UserEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
 	private String name;
 	
-	@ManyToOne
+	@OneToMany(targetEntity = Notification.class)
 	private List<Notification> notifications;
 	
 	private AccountType accountType;
 	
+	@OneToOne
 	private Course course;
 }

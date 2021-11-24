af2e3809c98be23d9ed1fde79a55e5a1f98ebc5a
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-11-24 12:38:17 -0300

    Course CRUD service implemented

---
+++ UniCalUPE-Backend/src/main/java/unicalApplication/services/CourseService.java
@@ -0,0 +1,49 @@
+package unicalApplication.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javassist.NotFoundException;
+import unicalApplication.models.Course;
+import unicalApplication.repositories.ICourseDAO;
+
+@Service
+public class CourseService {
+
+	@Autowired
+	ICourseDAO courseDAO;
+
+	public List<Course> getAll() throws NotFoundException {
+		List<Course> all = courseDAO.findAll();
+		if(all.isEmpty()) {
+			throw new NotFoundException("Nenhuma notificação encontrada");
+		}
+		return all;
+	}
+
+	public Course add(Course Course) {
+		return courseDAO.save(Course);
+	}
+
+	public Course findByID(Long id) throws NotFoundException {
+		Optional<Course> Course = courseDAO.findById(id);
+		if (Course.isEmpty())
+			throw new NotFoundException("Course não encontrado");
+		return Course.get();
+	}
+
+	public Course delete(Long id) throws NotFoundException {
+		Course Course = this.findByID(id);
+		courseDAO.delete(Course);
+		return Course;
+	}
+
+	public Course update(long id, Course Course) throws NotFoundException {
+		Course CourseInDb = this.findByID(id);
+		Course save = courseDAO.save(CourseInDb);
+		return save;
+	}
+}
